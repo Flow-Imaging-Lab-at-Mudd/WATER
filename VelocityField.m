e2d29ea3d77169c1c476e6b0aa2794b1be3f5489
Derek Li <lij@winchesterthurston.org>
2021-06-04 12:11:14 -0700

    global uniform random noise and plotting

--- VelocityField.m
+++ VelocityField.m
@@ -6,6 +6,10 @@ classdef VelocityField < handle
         X
         % 4D matrix of velocity vectors on grid.
         U
+        % Grid dimension.
+        dim
+        % Optional 4D matrix of noise in velocity to be superposed with U.
+        N
         
         % Derived fields.
         
@@ -39,6 +43,7 @@ classdef VelocityField < handle
             
             VF = VelocityField(X, U);
         end
+        
     end
     
     methods
@@ -46,6 +51,13 @@ classdef VelocityField < handle
         function obj = VelocityField(X, U)
             obj.X = X;
             obj.U = U;
+            if ~isequal(size(X), size(U))
+                error('Mismatching grid dimensions for position and velocity data')
+            end
+            dim = size(X);
+            obj.dim = dim(1:3);
+            % Init null additional noise.
+            N = zeros(size(U));
             obj.xbounds = [X(1,1,1,1) X(1,end,1,1)];
             % Suppose our data is not planar.
             obj.xresol = X(1,2,1,1) - X(1,1,1,1);
@@ -54,6 +66,32 @@ classdef VelocityField < handle
             obj.zbounds = [X(1,1,1,3) X(1,1,end,3)];
             obj.zresol = X(1,1,2,3) - X(1,1,1,3);
         end
+        
+        % Introduce species of noise.
+        function N = noise_uniform(vf, mag, in_place)
+            N = rand(size(vf.U))*mag;
+            if exist('in_place', 'var')
+                vf.N = N;
+                vf.plotNoisyVelocity();
+            end
+        end
+        
+        % Plotters
+        function plt = plotNoisyVelocity(vf, range)
+            if ~exist('range', 'var')
+                range = [ones(3, 1) vf.dim'];
+            end
+            plt = plotVF(vf.X, vf.U + vf.N, range);
+        end
+        
+        % Plotters
+        function plt = plotVelocity(vf, range)
+            if ~exist('range', 'var')
+                range = [ones(3, 1) vf.dim'];
+            end
+            plt = plotVF(vf.X, vf.U, range);
+        end
+        
         
     end
 end
