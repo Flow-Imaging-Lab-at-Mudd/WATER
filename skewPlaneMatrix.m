10a3f0bb76cd2d8e15008d241b07f50c08fa22ed
Derek Li <lij@winchesterthurston.org>
2021-06-07 15:49:02 -0700

    auto-dimensionalize allowed for plane predicate

--- skewPlaneMatrix.m
+++ skewPlaneMatrix.m
@@ -1,7 +1,10 @@
-function on = skewPlaneMatrix(X, orth, base)
-% SKEWPLANEMATRIX(X, orth, base) returns a 4D matrix corresponding to the
+function on = skewPlaneMatrix(X, orth, base, sz)
+% SKEWPLANEMATRIX(X, orth, base) returns a matrix corresponding to the
 % positions in X, indicating whether positions therein are on the plane
 % defined by the orthogonal vector and the base position given.
+%
+% sz is the size of the last dimension used for element-wise multiplication.
+% For velocity, sz = 4; for a scalar field, say error, sz = 1.
 
 % Tolerance for rounding error.
 tol = 1e-4;
@@ -20,8 +23,8 @@ Orth(:,:,:,3) = repmat(orth(3), dim);
 
 onplane = abs(dot(X - Base, Orth, 4)) < repmat(tol, dim);
 % Expand this logical 3D matrix indicating membership on plane to
-% 4D for direct point-wise multiplication with X.
-on = zeros(size(X));
-on(:,:,:,1) = onplane;
-on(:,:,:,2) = onplane;
-on(:,:,:,3) = onplane;
+% the size of the last dimension for direct point-wise multiplication.
+on = zeros([dim sz]);
+for i = 1: sz
+    on(:,:,:,i) = onplane;
+end
