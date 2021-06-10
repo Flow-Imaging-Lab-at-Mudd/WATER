dd9a048ae5bac0cd9f378b3614736338c23db818
Derek Li <lij@winchesterthurston.org>
2021-06-10 11:58:09 -0700

    l = 5 range: linear and oscillatory

--- trials/KE_uniform.m
+++ trials/KE_uniform.m
@@ -1,29 +1,26 @@
 vf = VelocityField.import_grid_separate(x,y,z,u,v,w);
 vf.setRange([27 30; 20 22; 10 12]);
-vf.data.speed = sqrt(sum(vf.U.^2, 4));
+speed = sqrt(sum(vf.U.^2, 4));
 
-% Maximal volume dimensions.
-vol_max = [5 5 5];
+% Minimal and maximal volume dimensions.
+vol = [3 5; 3 5; 3 5];
 
 % Introduce noise proportionally.
 props = (1:30) * 0.1;
-
+d
 % Randomly sample effective regions.
 num_ite = 20;
 for j = 1: num_ite
     % Random range.
-    range = [randi(vf.dims(2) - vol_max(1)); ...
-        randi(vf.dims(1) - vol_max(2)); ...
-        randi(vf.dims(3) - vol_max(3))];
-    range(:, 2) = range(:, 1) + [randi(vol_max(1)); randi(vol_max(2)); randi(vol_max(3))];
-    disp(range)
+    range = randRange(vf.dims, vol);
+    range
     vf.setRange(range)
     
     % Max speed in region of interest.
-    speed_r = vf.subsetVector(vf.data.speed);
+    speed_r = vf.subsetVector(speed);
     max_speed_r = max(speed_r, [], 'all');
     % Set constant maximal magnitude of noise.
-    err_mag = max_speed_r / 5;
+    err_mag = vf.meanSpeed(0) / 4;
     dK = zeros(size(props));
     
     % Kinetic energy without noise.
@@ -34,16 +31,30 @@ for j = 1: num_ite
         vf.noise_uniform(props(i)*err_mag);
         dK(i) = vf.kineticEnergy(1) - k;
     end
+    
+    % Plot KE error.
     figure;
-    subplot(1, 2, 1)
+    subplot(2, 2, 1)
     scatter(props(1:10), dK(1:10))
-    xlabel('$\delta u$ (per maximum speed)')
+    xlabel('$|\delta u|$ (per maximum speed)')
     ylabel('$\delta K$ (J)')
-    subplot(1, 2, 2)
+    subplot(2, 2, 2)
     scatter(props, dK)
-    xlabel('$\delta u$ (per maximum speed)')
+    xlabel('$|\delta u|$ (per maximum speed)')
     ylabel('$\delta K$ (J)')
-    title(strcat('Volume ', string(strjoin(string(range(:,2)-range(:,1))))))
+    
+    % Plot absolute KE error.
+    subplot(2, 2, 3)
+    scatter(props(1:10), abs(dK(1:10)))
+    xlabel('$|\delta u|$ (per maximum speed)')
+    ylabel('$|\delta K|$ (J)')
+    subplot(2, 2, 4)
+    scatter(props, abs(dK))
+    xlabel('$|\delta u|$ (per maximum speed)')
+    ylabel('$|\delta K|$ (J)')
+    
+    sgtitle(strcat('Volume:', {' '}, strjoin(string(range(:,2)-range(:,1))), ...
+        {'; '}, '$\bar{u} = $', string(vf.meanSpeed(0))))
     
     pause
     close
