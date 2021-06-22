31f6097b1959f6e8d4d088d4d23457913b364729
Derek Li <lij@winchesterthurston.org>
2021-06-22 16:35:34 -0700

    plotting error vs window size

--- trials/ke/windowing/KE_err_window_run.m
+++ trials/ke/windowing/KE_err_window_run.m
@@ -1,154 +1,139 @@
-function [dK, dKd, bias, err_mean] = ...
+function [dK, dKd, dK_box, dK_gss, bias_box, bias_gss, abs_err_mean_box, abs_err_mean_gss] = ...
     KE_err_window_run(vf0, winsize, overlap, props, fr)
 % Identical to KE_err_run.m except that error now by comparison of the
-% downsampled data to the correct original data 'vf0'. Further, as the
-% downsampling implemented involved averaging of velocities within window,
-% we apply no additional filter.
+% downsampled data to the correct original data 'vf0'. The accurate
+% velocity field is first downsampled, and then noise is introduced to the
+% downsampled field.
 
 
 % Presumed parameters: 'range', 'vf' with range properly set; and
 % occasionally 'fr' when a mean central speed is to be estimated for a
 % Hill's vortex.
 
-range0 = vf0.getRange();
-
-% % Set constant maximal magnitude of noise.
-% u_mean = vf0.meanSpeed(0, 0);
-
-% Use central speed for feature focusing. Enough resolution is presumed for
-% proper indexing.
-center = [floor(vf0.getDims()/2) - floor(fr / vf0.xresol); ...
-    floor(vf0.getDims()/2) + floor(fr / vf0.xresol)]';
-center(:, 1) = max([center(:,1) ones(3, 1)], [], 2);
-center(:, 2) = min([center(:,2) vf0.getDims()'], [], 2);
-vf0.setRange(center)
-u_mean = vf0.meanSpeed(0, 0);
-vf0.setRange(range0)
-
 % Correct kinetic energy computed from original data.
 k0 = vf0.kineticEnergy(0);
 
-% Error in energy estimation given noise.
-dK = zeros(size(props));
-% Error from downsampling.
-dKd = zeros(size(props));
+% Create downsampled field and compute error.
+vfd = vf0.downsample(winsize, overlap, 0);
+% Shift between noise-free KE vals.
+kd = vfd.kineticEnergy(0);
+% KE error from downsampling.
+dKd = kd - k0;
 
-% Plot energy estimation error for small and large values of noise.
-for i = 1: size(props, 2)
-    vf0.clearNoise();
-    N = vf0.noise_uniform(props(i)*u_mean);
-    
-    % Create downsampled field and compute error.
-    vfd = vf0.downsample(winsize, overlap, 1);
-    % Original KE noise.
-    dK(i) = vf0.kineticEnergy(1) - k0;
-    % Downsampled KE noise.
-    dKd(i) = vfd.kineticEnergy(0) - k0;
-end
+% Introduce error to the downsampled field. The positions are not altered
+% in downsampling.
+[dK, dK_box, dK_gss, ~, ~] = KE_err_run(vfd, props, fr);
 
-% Normalize by correct KE.
-dK = dK / k0;
+% Refashion errors with respect to KE from original field.
+dK = (kd*dK + dKd) / k0;
+abs_dK = abs(dK);
+
+dK_box = (kd*dK_box + dKd) / k0;
+abs_dK_box = abs(dK_box);
+
+dK_gss = (kd*dK_gss + dKd) / k0;
+abs_dK_gss = abs(dK_gss);
+
+bias_box = dK_box(1);
+bias_gss = dK_gss(1);
+
+err_mean_box = mean(dK_box);
+abs_err_mean_box = mean(abs(dK_box));
+
+err_mean_gss = mean(dK_gss);
+abs_err_mean_gss = mean(abs(dK_gss));
+
+% Now normalize sampling bias.
 dKd = dKd / k0;
 
-abs_dK = abs(dK);
-abs_dKd = abs(dKd);
-
-% Baseline smoother biases.
-bias = dKd(1);
-
-abs_bias = abs(bias);
-
-% Mean error of downsampling.
-err_mean = mean(dKd);
-
-abs_err_mean = abs(err_mean);
 
 %%%%%%%%%%%%%%%%% Plot KE error %%%%%%%%%%%%%%%%%%
 % figure;
 % scatter(props, dK, 'filled')
 % hold on
-% scatter(props, dKd, 'r', 'filled')
+% yline(dKd, 'g', '-')
 % hold on
-% yline(bias, '-')
+% scatter(props, dK_box, 'r', 'filled')
 % hold on
-% yline(err_mean, '-')
+% scatter(props, dK_gss, 'b', 'filled')
 % 
-% 
-% legend('unfiltered', 'downsampled', ...
-%     strcat('downsampling bias $\kappa = $', {' '}, string(bias)),  ...
-%     strcat('mean downsampling $\frac{\delta K}{K} = $', string(err_mean)), ...
+% legend('unfiltered downsampled error', ...
+%     strcat('downsampling bias $\delta = $', {' '}, string(dKd)), ...
+%     strcat('box-filtered, mean $\frac{\delta K}{K} = $', {' '}, string(err_mean_box)),  ...
+%     strcat('box-filtered, mean $\frac{\delta K}{K} = $', {' '}, string(err_mean_gss)), ...
 %     'Interpreter', 'latex')
 % xlabel('$\frac{|\delta u|}{\bar{u}}$')
 % ylabel('$\frac{\delta K}{K}$')
+% title(strcat('KE Error with Downsampling $\delta = $', ...
+%     {' '}, string(abs(dKd))))
 
 %%%%%%%%%%%%%%%%%% Plot absolute KE error %%%%%%%%%%%%%%%%%%%%%
-figure;
-scatter(props, abs_dK, 'filled')
-hold on
-scatter(props, abs_dKd, 'r', 'filled')
-hold on
-yline(abs_bias, '-')
-hold on
-yline(abs_err_mean, '-')
-
-legend('unfiltered', 'downsampled', ...
-    strcat('downsampling bias $\kappa = $', {' '}, string(abs(bias))),  ...
-    strcat('mean downsampling $\left|\frac{\delta K}{K}\right| = $', string(abs(err_mean))), ...
-    'Interpreter', 'latex')
-xlabel('$\frac{|\delta u|}{\bar{u}}$')
-ylabel('$|\frac{\delta K}{K}|$')
-
-%%%%%%%%%%%% KE error plot vs KE noise. %%%%%%%%%%%%
-figure;
-scatter(abs_dK, abs_dKd, 'r', 'filled')
-hold on
-% 1-1 line.
-plot(abs_dK, abs_dK, 'black')
-hold on
-yline(abs_bias, '-', 'Color', 'r')
-
-legend('downsampled', ...
-    'identity line', ...
-    strcat('downsampling bias $\kappa = $', string(abs_bias)), ...
-    'Interpreter', 'latex')
-
-xlabel('Unfiltered $|\frac{\delta K}{K}|$')
-ylabel('Downsampled $\frac{|\delta K|}{\bar{K}}$')
+% figure;
+% scatter(props, abs(dK), 'filled')
+% hold on
+% yline(abs(dKd), 'g', '-')
+% hold on
+% scatter(props, abs(dK_box), 'r', 'filled')
+% hold on
+% scatter(props, abs(dK_gss), 'b', 'filled')
+% 
+% legend('unfiltered downsampled error', ...
+%     strcat('downsampling bias $\delta = $', {' '}, string(abs(dKd))), ...
+%     strcat('box-filtered, mean $\left|\frac{\delta K}{K}\right| = $', {' '}, string(abs_err_mean_box)),  ...
+%     strcat('box-filtered, mean $\left|\frac{\delta K}{K}\right| = $', {' '}, string(abs_err_mean_gss)), ...
+%     'Interpreter', 'latex')
+% xlabel('$\frac{|\delta u|}{\bar{u}}$')
+% ylabel('$\left|\frac{\delta K}{K}\right|$')
+% title(strcat('Absolute KE Error with Downsampling $\delta = $', ...
+%     {' '}, string(abs(dKd))))
+% 
+%%%%%%%%%%%%%% KE error plot vs KE noise. %%%%%%%%%%%%
+% figure;
+% yline(abs(dKd), '-', 'Color', 'g')
+% hold on
+% % 1-1 line.
+% plot(abs_dK, abs_dK, 'black')
+% hold on
+% scatter(abs_dK, abs_dK_box, 'r', 'filled')
+% hold on
+% scatter(abs_dK, abs_dK_gss, 'b', 'filled')
+% 
+% legend('downsampling bias $\delta$', ...
+%     'identity line', ...
+%     strcat('box-filtered, bias $\kappa = $', string(abs(bias_box))), ...
+%     strcat('Gaussian-filtered, bias $\kappa = $', string(abs(bias_gss))), ...
+%     'Interpreter', 'latex')
+% 
+% xlabel('Unfiltered $|\frac{\delta K}{K}|$')
+% ylabel('$|\frac{\delta K}{\bar{K}}|$')
+% title(strcat('Filerting of KE Noise with Downsampling $\delta = $', ...
+%     {' '}, string(abs(dKd))))
 
 %%%%%%%%%%%%% Smoothing errors as proportion of smoother bias %%%%%%%%%%%%
-err_prop = abs(dKd / bias);
-% Fit and record quadratic curves. The quadratic coefficient can serve as a
-% measure of the KE error amplification rate.
-err_quad = polyfit(props, err_prop, 2);
-
-figure;
-scatter(props, err_prop, 'r', 'filled')
-hold on
-err_fit = polyplot(err_quad, props);
-
-
-legend(strcat('downsampling bias $\kappa = $', string(abs_bias)), ...
-    strcat('fit $r^2 = $', string(cor(err_fit, err_prop))), ...
-    'Interpreter', 'latex')
+% err_prop_box = abs(dK_box / bias_box);
+% err_prop_gss = abs(dK_gss / bias_gss);
+% % Fit and record quadratic curves. The quadratic coefficient can serve as a
+% % measure of the KE error amplification rate.
+% err_quad_box = polyfit(props, err_prop_box, 2);
+% err_quad_gss = polyfit(props, err_prop_gss, 2);
 % 
-% % Different test dataset run.
-% % dataset_str = 'Turbulent Vortex Ring';
-% dataset_str = 'Synthetic Hill Vortex';
+% figure;
+% scatter(props, err_prop_box, 'r', 'filled')
+% hold on
+% err_fit_box = polyplot(err_quad_box, props, 'r');
+% hold on
+% scatter(props, err_prop_gss, 'b', 'filled')
+% hold on
+% err_fit_gss = polyplot(err_quad_gss, props, 'b');
 % 
 % 
-% title(strcat('Error Proportional to Smoother Bias:', {' '}, dataset_str))
+% legend(strcat('box-filtered, $\kappa = $', string(abs(bias_box))), ...
+%     strcat('box fit $r^2 = $', string(cor(err_fit_box, err_prop_box))), ...
+%     strcat('Gaussian-filtered, $\kappa = $', string(abs(bias_gss))), ...
+%     strcat('Gaussian fit $r^2 = $', string(cor(err_fit_gss, err_prop_gss))), ...
+%     'Interpreter', 'latex')
 % 
 % xlabel('$\frac{|\delta u|}{\bar{u}}$')
 % ylabel('$\frac{\left|\frac{\delta K}{K}\right|}{\kappa}$')
-
-
-% uerr_histogram(vf.N_e);
-
-% vf.plotVector(vf.U_e, 0, strcat(range_str, {' '}, '$\bar{u} = $', string(u_mean)));
-
-% vf.plotScalar(sqrt(sum(vf.N_e.^2, 4)), 0, '');
-
-% plane_range = range;
-% % Plot a parallel xy plane.f
-% plane_range(3, 2) = plane_range(3, 1);
-% vf.plotPlaneScalar(sqrt(sum(vf.N.^2, 4)), plane_range, 0, 'noise $\Delta u$')
+% title(strcat('Error Proportional to Smoother Bias with Downsampling $\delta = $', ...
+%     {' '}, string(abs(dKd))))
--- trials/ke/windowing/err_v_winsize.m
+++ trials/ke/windowing/err_v_winsize.m
@@ -1,13 +1,13 @@
 % Original vortex used.
-sp = 0.05;
+sp = 0.02;
 fr = 1;
 [x, y, z, u, v, w, Mag] = hill_vortex_3D(sp, fr, 1, 1);
 vf = VelocityField.import_grid_separate(x,y,z,u,v,w);
 % Subtract freestream velocity.
 vf.addVelocity(-vf.U(1,1,1,:))
 
-% Uniform windows.
-windows = 2: 11;
+% Uniform windows in x, y, z.
+windows = 2.^(1: 6);
 windows_count = size(windows, 2);
 
 % Constant overlap used.
@@ -19,19 +19,36 @@ props_count = size(props, 2);
 
 % Containers for error data at different window sizes.
 dK = zeros(props_count, windows_count);
-dKd = zeros(props_count, windows_count);
-bias = zeros(1, windows_count);
-mean_err = zeros(1, windows_count);
+dKd = zeros(1, windows_count);
+dK_box = zeros(props_count, windows_count);
+dK_gss = zeros(props_count, windows_count);
+bias_box = zeros(1, windows_count);
+bias_gss = zeros(1, windows_count);
+mean_abs_err_box = zeros(1, windows_count);
+mean_abs_err_gss = zeros(1, windows_count);
 
 for i = 1: windows_count
     winsize = windows(i);
-    [dK(:,i), dKd(:,i), bias(i), mean_err(i)] = ...
+    [dK(:,i), dKd(i), dK_box(:,i), dK_gss(:,i), bias_box(i), bias_gss(i), ...
+        mean_abs_err_box(i), mean_abs_err_gss(i)] = ...
         KE_err_window_run(vf, winsize, overlap, props, fr);
 end
 
+% Plot of sampling bias, expected monotonic increase of underestimation.
+figure;
+scatter(windows, dKd, 'k', 'filled')
+
 % Plot of bias.
 figure;
-scatter(windows, abs(bias), 'r', 'filled')
+scatter(windows, abs(dKd), 'k', 'filled')
+hold on
+scatter(windows, abs(bias_box), 'r', 'filled')
+hold on
+scatter(windows, abs(bias_gss), 'b', 'filled')
+
+legend(strcat('downsampling bias $\delta$'), ...
+    strcat('box bias'),  ...
+    strcat('Gaussian bias'), 'Interpreter', 'latex')
 xlabel('Window Size $w$')
 ylabel('$\kappa$')
 title(strcat('Downsampling biases at overlap $o=$', {' '}, string(overlap)))
@@ -42,10 +59,14 @@ img_fdr = strcat('C:\Users\derek\flow\trials\ke\windowing\window-size\o=', ...
 mkdir(img_fdr);
 saveas(gcf, strcat(img_fdr, 'bias-', string(overlap), 'o.jpg'));
 
-% Plot of mean errors.
+% Plot of mean absolute errors.
 figure;
-scatter(windows, mean_err, 'r', 'filled')
+scatter(windows, mean_abs_err_box, 'r', 'filled')
+hold on
+scatter(windows, mean_abs_err_gss, 'b', 'filled')
+
+legend('box-filtered', 'Gaussian-filtered')
 xlabel('Window Size $w$')
 ylabel('$\left|\frac{\delta K}{K}\right|$')
-title(strcat('Mean smoother errors at overlap $o=$', {' '}, string(overlap)))
+title(strcat('Mean Smoother Errors at Overlap $o=$', {' '}, string(overlap)))
 saveas(gcf, strcat(img_fdr, 'err-', string(overlap), 'o.jpg'));
