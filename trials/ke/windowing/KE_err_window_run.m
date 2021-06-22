cffcb7e9dff4dde4fc14122be50137366bf60313
Derek Li <lij@winchesterthurston.org>
2021-06-21 22:17:41 -0700

    windowing plots

--- trials/ke/windowing/KE_err_window_run.m
+++ trials/ke/windowing/KE_err_window_run.m
@@ -1,4 +1,4 @@
-function [dK, dKd, dK_box, dK_gss, bias_box, bias_gss] = ...
+function [dK, dKd, bias, err_mean] = ...
     KE_err_window_run(vf0, winsize, overlap, props, fr)
 % Identical to KE_err_run.m except that error now by comparison of the
 % downsampled data to the correct original data 'vf0'. Further, as the
@@ -30,16 +30,8 @@ k0 = vf0.kineticEnergy(0);
 
 % Error in energy estimation given noise.
 dK = zeros(size(props));
-% Error from downsampling without smoothing.
+% Error from downsampling.
 dKd = zeros(size(props));
-% Box smoothing.
-dK_box = zeros(size(props));
-% Gaussian smoothing.
-dK_gss = zeros(size(props));
-
-% % KE error profiles per point.
-% dK_pro_box = zeros(vf.span);
-% dK_pro_gss = zeros(vf.span);
 
 % Plot energy estimation error for small and large values of noise.
 for i = 1: size(props, 2)
@@ -52,135 +44,92 @@ for i = 1: size(props, 2)
     dK(i) = vf0.kineticEnergy(1) - k0;
     % Downsampled KE noise.
     dKd(i) = vfd.kineticEnergy(0) - k0;
-    
-    % Results with smoothing.
-    vfd.smoothVelocity('box');
-    dK_box(i) = vfd.kineticEnergy(0) - k0;
-%    dK_pro_box = abs(KE_profile(vf, 1) - KE_profile(vf, 0)) / k;
-    vfd.smoothVelocity('gaussian');
-    dK_gss(i) = vfd.kineticEnergy(0) - k0;
-%    dK_pro_gss = abs(KE_profile(vf, 1) - KE_profile(vf, 0)) / k;
-    
 end
 
 % Normalize by correct KE.
 dK = dK / k0;
 dKd = dKd / k0;
-dK_box = dK_box / k0;
-dK_gss = dK_gss / k0;
 
+abs_dK = abs(dK);
+abs_dKd = abs(dKd);
 
 % Baseline smoother biases.
-bias_box = dK_box(1);
-bias_gss = dK_gss(1);
+bias = dKd(1);
+
+abs_bias = abs(bias);
+
+% Mean error of downsampling.
+err_mean = mean(dKd);
+
+abs_err_mean = abs(err_mean);
 
 %%%%%%%%%%%%%%%%% Plot KE error %%%%%%%%%%%%%%%%%%
-figure;
-scatter(props, dK, 'filled')
-hold on
-scatter(props, dKd, 'g', 'filled')
-hold on
-scatter(props, dK_box, 'r', 'filled')
-hold on
-err_mean_box = mean(dK_box);
-yline(err_mean_box, '-')
-hold on
-scatter(props, dK_gss, 'b', 'filled')
-hold on
-err_mean_gss = mean(dK_gss);
-yline(err_mean_gss, '-')
-
-legend('unfiltered $\delta K$', 'unfiltered \& downsampled', ...
-    'box-filtered $\vec{u}$',  ...
-    strcat('box $\frac{\delta K}{K} = $', string(err_mean_box)), ...
-    'Gaussian-filtered $\vec{u}$', ...
-    strcat('Gaussian $\frac{\delta K}{K} = $', string(err_mean_gss)), ...
-    'Interpreter', 'latex')
-xlabel('$\frac{|\delta u|}{\bar{u}}$')
-ylabel('$\frac{\delta K}{K}$')
-% title(range_str)
+% figure;
+% scatter(props, dK, 'filled')
+% hold on
+% scatter(props, dKd, 'r', 'filled')
+% hold on
+% yline(bias, '-')
+% hold on
+% yline(err_mean, '-')
+% 
+% 
+% legend('unfiltered', 'downsampled', ...
+%     strcat('downsampling bias $\kappa = $', {' '}, string(bias)),  ...
+%     strcat('mean downsampling $\frac{\delta K}{K} = $', string(err_mean)), ...
+%     'Interpreter', 'latex')
+% xlabel('$\frac{|\delta u|}{\bar{u}}$')
+% ylabel('$\frac{\delta K}{K}$')
 
 %%%%%%%%%%%%%%%%%% Plot absolute KE error %%%%%%%%%%%%%%%%%%%%%
 figure;
-scatter(props, abs(dK), 'filled')
+scatter(props, abs_dK, 'filled')
 hold on
-scatter(props, abs(dKd), 'g', 'filled')
+scatter(props, abs_dKd, 'r', 'filled')
 hold on
-scatter(props, abs(dK_box), 'r', 'filled')
+yline(abs_bias, '-')
 hold on
-abs_err_mean_box = mean(abs(dK_box));
-yline(abs_err_mean_box, '-')
-hold on
-scatter(props, abs(dK_gss), 'b', 'filled')
-hold on
-abs_err_mean_gss = mean(abs(dK_gss));
-yline(abs_err_mean_gss, '-')
+yline(abs_err_mean, '-')
 
-legend('unfiltered $|\delta K|$', ...
-    'downsampled', ...
-    'box-filtered $\vec{u}$', ...
-    strcat('box $\left|\frac{\delta K}{K}\right| = $', ...
-    string(abs_err_mean_box)), ...
-    'Gaussian-filtered $\vec{u}$', ...
-    strcat('Gaussian $\left|\frac{\delta K}{K}\right| = $', string(abs_err_mean_gss)), ...
+legend('unfiltered', 'downsampled', ...
+    strcat('downsampling bias $\kappa = $', {' '}, string(abs(bias))),  ...
+    strcat('mean downsampling $\left|\frac{\delta K}{K}\right| = $', string(abs(err_mean))), ...
     'Interpreter', 'latex')
-%     strcat('box bias $\kappa = $', string(abs(bias_box))), ...
-%     strcat('Gaussian bias $\kappa = $', string(abs(bias_gss))), ...
 xlabel('$\frac{|\delta u|}{\bar{u}}$')
 ylabel('$|\frac{\delta K}{K}|$')
-% title(range_str)
 
 %%%%%%%%%%%% KE error plot vs KE noise. %%%%%%%%%%%%
-% figure;
-% scatter(abs(dK), abs(dK_box), 'r', 'filled')
-% hold on
-% scatter(abs(dK), abs(dK_gss), 'b', 'filled')
-% hold on
-% % 1-1 line.
-% plot(dK, dK, 'black')
-% hold on
-% % Smoother biases.
-% abs_bias_box = abs(bias_box);
-% yline(abs_bias_box, '-', 'Color', 'r')
-% 
-% hold on
-% abs_bias_gss = abs(bias_gss);
-% yline(abs_bias_gss, '-', 'Color', 'b')
-% 
-% legend('box-filtered', 'Gaussian-filtered', ...
-%     '$y=x$ identity line', ...
-%     strcat('box bias $\kappa = $', string(abs_bias_box)), ...
-%     strcat('Gaussian bias $\kappa = $', string(abs_bias_gss)), ...
-%     'Interpreter', 'latex')
-% 
-% xlabel('Unfiltered $|\frac{\delta K}{K}|$')
-% ylabel('Filtered $\frac{|\delta K|}{\bar{K}}$')
-% title(range_str)
+figure;
+scatter(abs_dK, abs_dKd, 'r', 'filled')
+hold on
+% 1-1 line.
+plot(abs_dK, abs_dK, 'black')
+hold on
+yline(abs_bias, '-', 'Color', 'r')
+
+legend('downsampled', ...
+    'identity line', ...
+    strcat('downsampling bias $\kappa = $', string(abs_bias)), ...
+    'Interpreter', 'latex')
+
+xlabel('Unfiltered $|\frac{\delta K}{K}|$')
+ylabel('Downsampled $\frac{|\delta K|}{\bar{K}}$')
 
 %%%%%%%%%%%%% Smoothing errors as proportion of smoother bias %%%%%%%%%%%%
-% err_prop_box = abs(dK_box / bias_box);
-% err_prop_gss = abs(dK_gss / bias_gss);
-% % Fit and record quadratic curves. The quadratic coefficient can serve as a
-% % measure of the KE error amplification rate.
-% err_quad_box = polyfit(props, err_prop_box, 2);
-% err_quad_gss = polyfit(props, err_prop_gss, 2);
-% 
-% figure;
-% scatter(props, err_prop_box, 'r', 'filled')
-% hold on
-% err_fit_box = polyplot(err_quad_box, props);
-% 
-% hold on
-% scatter(props, err_prop_gss, 'b', 'filled')
-% hold on
-% err_fit_gss = polyplot(err_quad_gss, props);
-% 
-% 
-% legend(strcat('box $\kappa = $', string(abs(bias_box))), ...
-%     strcat('box fit $r^2 = $', string(cor(err_fit_box, err_prop_box))), ...
-%     strcat('Gaussian $\kappa = $', string(abs(bias_gss))), ...
-%     strcat('Gaussian fit $r^2 = $', string(cor(err_fit_gss, err_prop_gss))), ...
-%     'Interpreter', 'latex')
+err_prop = abs(dKd / bias);
+% Fit and record quadratic curves. The quadratic coefficient can serve as a
+% measure of the KE error amplification rate.
+err_quad = polyfit(props, err_prop, 2);
+
+figure;
+scatter(props, err_prop, 'r', 'filled')
+hold on
+err_fit = polyplot(err_quad, props);
+
+
+legend(strcat('downsampling bias $\kappa = $', string(abs_bias)), ...
+    strcat('fit $r^2 = $', string(cor(err_fit, err_prop))), ...
+    'Interpreter', 'latex')
 % 
 % % Different test dataset run.
 % % dataset_str = 'Turbulent Vortex Ring';
