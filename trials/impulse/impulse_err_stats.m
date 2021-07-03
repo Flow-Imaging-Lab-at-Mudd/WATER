c7e3271f5386b807eb6649b03803693f41209e63
Derek Li <lij@winchesterthurston.org>
2021-07-02 23:04:16 -0700

    adding original error in return

--- trials/impulse/impulse_err_stats.m
+++ trials/impulse/impulse_err_stats.m
@@ -1,12 +1,12 @@
 function [dI_mean, dI_sd, dI_mean_box, dI_sd_box, dI_mean_gss, dI_sd_gss, ...
-    bias_box, bias_gss] = impulse_err_stats(vf, props, origin, fr, u0)
+    bias_box, bias_gss, dI0] = impulse_err_stats(vf, props, origin, fr, u0)
 % Extract sample statistics so that the error is represented at a lower
 % dimension.
 
 % Complete error data.
 [dI, dI_box, dI_gss, bias_box, bias_gss] = impulse_err_run(vf, props, origin, fr, u0);
 
-% Consider lower absolute error.
+% Consider only absolute error.
 abs_dI = abs(dI);
 dI_mean = mean(abs_dI, 2);
 dI_sd = std(abs_dI, 0, 2);
@@ -18,6 +18,10 @@ dI_sd_box = std(abs_dI_box, 0, 2);
 abs_dI_gss = abs(dI_gss);
 dI_mean_gss = mean(abs_dI_gss, 2);
 dI_sd_gss = std(abs_dI_gss, 0, 2);
+
+% Error due to imperfect resolution (perfect being infinite) and origin
+% selection.
+dI0 = abs_dI(:,1);
 
 bias_box = abs(bias_box);
 bias_gss = abs(bias_gss);
