054453a597a630fbcb6f21e0580ebf196bc4eac7
Derek Li <31416815+epicderek@users.noreply.github.com>
2021-11-20 21:06:48 -0800

    Remove up-dimensionalization helper.

--- dimen.m
+++
@@ -1,9 +0,0 @@
-function V = dimen(v, dims)
-% Replicate vector v to have preceding dimensions as specified by dims(1) x
-% dims(2) x dims(3) x length(v). Size of dimension is upposed to be 3.
-
-V = zeros([dims length(v)]);
-
-for i = 1: length(v)
-    V(:,:,:,i) = repmat(v(i), dims);
-end
