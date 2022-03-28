2b2c0f79d759927c6c757cd28ec5b275fb205488
Derek Li <lij@winchesterthurston.org>
2022-03-27 22:11:49 -0700

    updated comments

--- PIV_window_sim.m
+++ PIV_window_sim.m
@@ -1,4 +1,4 @@
-% This function simulates 3DPIV windowing effects (i.e. converts from voxel
+% This function simulates 3D PIV windowing effects (i.e. converts from voxel
 % to interrogation window scale)
 % Written by Leah Mendelson & Derek Li
 
@@ -10,9 +10,11 @@ function [Xwin, Uwin] = PIV_window_sim(X, U, winsize, overlap, Xscale)
 % 2 (typical range 16-128 voxels). Can be given a uniform scalar value
 % or 3-vector of x y z window sizes.
 % overlap: Overlap between interrogation windows as a fraction of the window
-% size in voxels (typically 0.5 or 0.75). Code assumes a uniform
-% overlap in each direction
-% Xscale: Scalar for how the subsetted positions are to be scaled.
+% size in voxels (typically 0.5 or 0.75). A uniform overlap in each direction
+% is assumed.
+% Xscale: Vector for how the subsetted positions are to be scaled, where
+% all positions will be non-negative and proceed from (0 0 0) by the
+% increments specified by 'Xscale'.
 %
 % outputs
 % Xwin: output vector field coordinates (in pixels, analogous
@@ -37,7 +39,6 @@ zdim = size(X, 3);
 overlap = round(winsize .* overlap);
 overlap = min([overlap; winsize-1], [], 1);
 
-
 % Box averaging of velociy values.
 % after this step, vectors are still on input grid, but filtered like
 % PIV processing.
@@ -61,7 +62,8 @@ if min(winsize) >= 1
     zdim_d = floor((zdim - winsize(3)) / shift_z) + 1;
 
     % Edge handling
-    % Last left coordinate for which enough data is available for a complete interrogation window of size boxes(dc)
+    % Last left coordinate for which enough data is available for a
+    % complete interrogation window。
     endIndex_x = shift_x * (xdim_d-1) + 1;
     endIndex_y = shift_y * (ydim_d-1) + 1;
     endIndex_z = shift_z * (zdim_d-1) + 1;
@@ -84,7 +86,7 @@ if min(winsize) >= 1
                 X(1,1,zrange,3) + (X(1,1,winsize(3),3)-X(1,1,1,3))/2);
     else
         [Xwin(:,:,:,1), Xwin(:,:,:,2), Xwin(:,:,:,3)] = ...
-            meshgrid(xrange*Xscale(1), yrange*Xscale(2), zrange*Xscale(3));
+            meshgrid((xrange-1)*Xscale(1), (yrange-1)*Xscale(2), (zrange-1)*Xscale(3));
     end
 else
     error('Window size must be larger than 1 voxel!')
