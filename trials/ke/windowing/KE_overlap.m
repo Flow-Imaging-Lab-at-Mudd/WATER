6960ecd96f2766a9cf2da32c73e6d36bbea5f8d2
Derek Li <lij@winchesterthurston.org>
2022-04-17 20:52:11 -0700

    retain sign of error

---
+++ trials/ke/windowing/KE_overlap.m
@@ -0,0 +1,122 @@
+function [dK, dK_box, dK_gss, dK0, bias_box, bias_gss, dK_sd, dK_sd_box, dK_sd_gss] = ...
+    KE_overlap(vf, K0, KEf, props, window, overlaps, display_plots)
+% Vary the overlap used in downsampling and present its effect on error.
+%
+% April, 2022
+
+if ~isvector(overlaps)
+    error('Uniform overlaps ratios expected!')
+end
+
+% Assume uniform windows in x, y, z.
+ops_count = length(overlaps);
+
+% Number of repetitions for noise trial.
+num_ite = 10;
+
+% Errors with noise.
+dK = zeros(ops_count, 1);
+dK_sd = zeros(ops_count, 1);
+dK_box = zeros(ops_count, 1);
+dK_sd_box = zeros(ops_count, 1);
+dK_gss = zeros(ops_count, 1);
+dK_sd_gss = zeros(ops_count, 1);
+
+% Resolution errors.
+bias_box = zeros(ops_count, 1);
+bias_gss = zeros(ops_count, 1);
+dK0 = zeros(ops_count, 1);
+
+for k = 1: ops_count
+    [dK(k), dK_box(k), dK_gss(k), dK0(k), bias_box(k), bias_gss(k), ...
+        dK_sd(k), dK_sd_box(k), dK_sd_gss(k), vfd] = ...
+            KE_err_run_constN(vf, props, K0, KEf, num_ite, [window, overlaps(k)]);
+        disp(vfd.dims)
+end
+
+%%%%%%%%%%% Error plots %%%%%%%%%%%%%%
+if ~exist('display_plots', 'var') || ~display_plots
+    return
+end
+
+% Whether error is to be plotted with sign (can be both).
+plot_signed = 1;
+plot_abs = 0;
+
+% Plot of windowing error.
+if plot_signed
+    figure;
+    scatter(overlaps, dK0, 'k', 'filled')
+    xticks(overlaps)
+    xlabel('Overlap ratio of windowing')
+    ylabel('Normalized error')
+    title('Windowing resolution error of KE')
+end
+
+if plot_abs
+    figure;
+    scatter(overlaps, abs(dK0), 'k', 'filled')
+    xticks(overlaps)
+    xlabel('Overlap ratio of windowing')
+    ylabel('Normalized error')
+    title('Absolute windowing resolution error of KE')
+end
+
+% Plot of bias.
+if plot_signed
+    figure;
+    scatter(overlaps, dK0, 'k', 'filled')
+    hold on
+    scatter(overlaps, bias_box, 'r', 'filled')
+    hold on
+    scatter(overlaps, bias_gss, 'b', 'filled')
+    xticks(overlaps)
+    legend({'windowing', 'box', 'Gaussian'})
+    xlabel('Overlap ratio of windowing')
+    ylabel('Normalized error')
+    title('Baseline KE resolution error of filters')
+end
+
+if plot_abs
+    figure;
+    scatter(overlaps, abs(dK0), 'k', 'filled')
+    hold on
+    scatter(overlaps, abs(bias_box), 'r', 'filled')
+    hold on
+    scatter(overlaps, abs(bias_gss), 'b', 'filled')
+    xticks(overlaps)
+    legend({'windowing', 'box', 'Gaussian'})
+    xlabel('Overlap ratio of windowing')
+    ylabel('Normalized error')
+    title('Absolute baseline KE resolution error of filters')
+end
+
+% Plot of mean absolute errors.
+if plot_signed
+    figure;
+    errorbar(overlaps, dK, dK_sd, 'ko', 'MarkerFaceColor', 'black', 'LineWidth', 1)
+    hold on
+    errorbar(overlaps, dK_box, dK_sd_box, 'ko', 'MarkerFaceColor', 'red', 'LineWidth', 1)
+    hold on
+    errorbar(overlaps, dK_gss, dK_sd_gss, 'ko', 'MarkerFaceColor', 'blue', 'LineWidth', 1)
+    xticks(overlaps)
+    legend({'unfiltered', 'box', 'Gaussian'})
+    xlabel('Overlap ratio of windowing')
+    ylabel('Normalized error')
+    title('KE Error with noise')
+end
+
+if plot_abs
+    figure;
+    errorbar(overlaps, abs(dK), dK_sd, 'ko', 'MarkerFaceColor', 'black', 'LineWidth', 1)
+    hold on
+    errorbar(overlaps, abs(dK_box), dK_sd_box, 'ko', 'MarkerFaceColor', 'red', 'LineWidth', 1)
+    hold on
+    errorbar(overlaps, abs(dK_gss), dK_sd_gss, 'ko', 'MarkerFaceColor', 'blue', 'LineWidth', 1) 
+    xticks(overlaps)
+    legend({'unfiltered', 'box', 'Gaussian'})
+    xlabel('Overlap ratio of windowing')
+    ylabel('Normalized error')
+    title('KE Error with noise')
+end
+
--- trials/ke/windowing/KE_winsize.m
+++ trials/ke/windowing/KE_winsize.m
@@ -1,134 +1,124 @@
-% Original vortex used.
-sp = 0.02;
-fr = 1;
-[x, y, z, u, v, w, Mag] = hill_vortex_3D(sp, fr, 1, 1);
-vf = VelocityField.import_grid_separate(x,y,z,u,v,w);
-% Subtract freestream velocity.
-vf.addVelocity(-vf.U(1,1,1,:))
+function [dK, dK_box, dK_gss, dK0, bias_box, bias_gss, dK_sd, dK_sd_box, dK_sd_gss] = ...
+    KE_winsize(vf, K0, KEf, props, windows, overlap, display_plots)
+% Vary the window size used in downsampling and present its effect on
+% error. The errors returned here are not absolute but the sign is
+% retained.
 
-% Zoom in on vortical region.
-vf.setRangePosition(fr * repmat([-1 1], 3, 1))
-
-% Uniform windows in x, y, z.
-windows = 2.^(1: 5);
-windows_count = size(windows, 2);
-
-% Constant overlap used.
-overlap = 0.75;
-
-% Proportional noise.
-props = 0: 0.1: 3;
-props_count = size(props, 2);
-
-% Containers for error data at different window sizes.
-dK = zeros(props_count, windows_count);
-dKd = zeros(1, windows_count);
-dK_box = zeros(props_count, windows_count);
-dK_gss = zeros(props_count, windows_count);
-bias_box = zeros(1, windows_count);
-bias_gss = zeros(1, windows_count);
-mean_abs_err_box = zeros(1, windows_count);
-mean_abs_err_gss = zeros(1, windows_count);
-
-% Minimum feature resolution to reduce error beneath an error level.
-err_level = 0.1;
-min_bias_fres = zeros(2, windows_count);
-min_err_fres = zeros(2, windows_count);
-% Feature resolutions tried.
-max_fres = 12;
-min_fres = 1;
-% Increment.
-fres_inc = 1;
-
-for i = 1: windows_count
-    winsize = windows(i);
-    [dK(:,i), dKd(i), dK_box(:,i), dK_gss(:,i), bias_box(i), bias_gss(i), ...
-        mean_abs_err_box(i), mean_abs_err_gss(i), ~] = ...
-        KE_err_window_run(vf, winsize, overlap, props);
-    % Compute the minimum feature resolution required.
-    [min_bias_fres(:,i), min_err_fres(:,i)] = ...
-        KE_window_resol(winsize, overlap, min_fres:fres_inc:max_fres, ...
-        err_level, props);
+if ~isvector(windows)
+    error('Uniform windows expected!')
 end
 
-% Plot of sampling bias, expected monotonic increase of underestimation.
-figure;
-scatter(windows, dKd, 'k', 'filled')
-xlabel('Window Size $w$')
-ylabel('$\kappa$')
-title('Downsampling Bias')
+% Assume uniform windows in x, y, z.
+windows_count = length(windows);
+
+% Number of repetitions for noise trial.
+num_ite = 10;
+
+% Errors with noise.
+dK = zeros(windows_count, 1);
+dK_sd = zeros(windows_count, 1);
+dK_box = zeros(windows_count, 1);
+dK_sd_box = zeros(windows_count, 1);
+dK_gss = zeros(windows_count, 1);
+dK_sd_gss = zeros(windows_count, 1);
+
+% Resolution errors.
+bias_box = zeros(windows_count, 1);
+bias_gss = zeros(windows_count, 1);
+dK0 = zeros(windows_count, 1);
+
+for k = 1: windows_count
+    [dK(k), dK_box(k), dK_gss(k), dK0(k), bias_box(k), bias_gss(k), ...
+        dK_sd(k), dK_sd_box(k), dK_sd_gss(k)] = ...
+            KE_err_run_constN(vf, props, K0, KEf, num_ite, [windows(k), overlap]);
+end
+
+%%%%%%%%%%% Error plots %%%%%%%%%%%%%%
+if ~exist('display_plots', 'var') || ~display_plots
+    return
+end
+
+% Whether error is to be plotted with sign (can be both).
+plot_signed = 1;
+plot_abs = 0;
+
+ % Plot of windowing error.
+if plot_signed
+    figure;
+    scatter(windows, dK0, 'k', 'filled')
+    xticks(windows)
+    xlabel('Window size')
+    ylabel('Normalized error')
+    title('Windowing resolution error of KE')
+end
+
+if plot_abs
+    figure;
+    scatter(windows, abs(dK0), 'k', 'filled')
+    xticks(windows)
+    xlabel('Window size')
+    ylabel('Normalized error')
+    title('Absolute windowing resolution error of KE')
+end
 
 % Plot of bias.
-figure;
-scatter(windows, abs(dKd), 'k', 'filled')
-hold on
-scatter(windows, abs(bias_box), 'r', 'filled')
-hold on
-scatter(windows, abs(bias_gss), 'b', 'filled')
+if plot_signed
+    figure;
+    scatter(windows, dK0, 'k', 'filled')
+    hold on
+    scatter(windows, bias_box, 'r', 'filled')
+    hold on
+    scatter(windows, bias_gss, 'b', 'filled')
+    
+    xticks(windows)
+    legend({'windowing', 'box', 'Gaussian'})
+    xlabel('Window size')
+    ylabel('Normalized error')
+    title('Baseline KE resolution error of filters')
+end
 
-bias_line_d = polyfit(windows, abs(dKd), 1);
-bias_line_box = polyfit(windows, abs(bias_box), 1);
-bias_line_gss = polyfit(windows, abs(bias_gss), 1);
+if plot_abs
+    figure;
+    scatter(windows, abs(dK0), 'k', 'filled')
+    hold on
+    scatter(windows, abs(bias_box), 'r', 'filled')
+    hold on
+    scatter(windows, abs(bias_gss), 'b', 'filled')
+    
+    xticks(windows)
+    legend({'windowing', 'box', 'Gaussian'})
+    xlabel('Window size')
+    ylabel('Normalized error')
+    title('Absolute baseline KE resolution error of filters')
+end
 
-hold on
-bias_fit_d = polyplot(bias_line_d, windows, 'k');
-hold on
-bias_fit_box = polyplot(bias_line_box, windows, 'r');
-hold on
-bias_fit_gss = polyplot(bias_line_gss, windows, 'b');
+% Plot of mean errors.
+if plot_signed
+    figure;
+    errorbar(windows, dK, dK_sd, 'ko', 'MarkerFaceColor', 'black', 'LineWidth', 1)
+    hold on
+    errorbar(windows, dK_box, dK_sd_box, 'ko', 'MarkerFaceColor', 'red', 'LineWidth', 1)
+    hold on
+    errorbar(windows, dK_gss, dK_sd_gss, 'ko', 'MarkerFaceColor', 'blue', 'LineWidth', 1)
+    
+    xticks(windows)
+    legend({'unfiltered', 'box', 'Gaussian'})
+    xlabel('Window size')
+    ylabel('Normalized error')
+    title('KE error with noise')
+end
 
-
-legend(strcat('downsampling bias $\delta$'), ...
-    strcat('box bias'),  ...
-    strcat('Gaussian bias'), ...
-    strcat('downsampling fit $r^2 = $', {' '}, string(cor(bias_fit_d, abs(dKd)))), ...
-    strcat('box fit $r^2 = $', {' '}, string(cor(bias_fit_box, abs(bias_box)))), ...
-    strcat('Gaussian fit $r^2 = $', {' '}, string(cor(bias_fit_gss, abs(bias_gss)))), ...
-    'Interpreter', 'latex')
-xlabel('Window Size $w$')
-ylabel('$\kappa$')
-title(strcat('Downsampling biases at overlap $o=$', {' '}, string(overlap)))
-
-% Save plots.
-img_fdr = strcat('C:\Users\derek\flow\trials\ke\windowing\window-size\o=', ...
-    string(overlap), '\');
-mkdir(img_fdr);
-% saveas(gcf, strcat(img_fdr, 'bias-', string(overlap), 'o.jpg'));
-
-% Plot of mean absolute errors.
-figure;
-scatter(windows, mean_abs_err_box, 'r', 'filled')
-hold on
-scatter(windows, mean_abs_err_gss, 'b', 'filled')
-
-legend('box-filtered', 'Gaussian-filtered')
-xlabel('Window Size $w$')
-ylabel('$\left|\frac{\delta K}{K}\right|$')
-title(strcat('Mean Smoother Errors at Overlap $o=$', {' '}, string(overlap)))
-% saveas(gcf, strcat(img_fdr, 'err-', string(overlap), 'o.jpg'));
-
-%%%%%% Make graphs for required minimum feature resolution.
-% Bias graph.
-figure;
-scatter(windows, min_bias_fres(1, :), 'r', 'filled')
-hold on
-scatter(windows, min_bias_fres(2, :), 'b', 'filled')
-
-legend({'box-filtered', 'Gaussian-filtered'}, 'Interpreter', 'latex')
-xlabel('Window Size $w$')
-ylabel('$\frac{r}{s}$')
-title(strcat('Minimum feature resolution for', ...
-    {' '}, string(err_level*100), '\% smoother bias'))
-
-% Mean error graph.
-figure;
-scatter(windows, min_err_fres(1, :), 'r', 'filled')
-hold on
-scatter(windows, min_err_fres(2, :), 'b', 'filled')
-
-legend({'box-filtered', 'Gaussian-filtered'}, 'Interpreter', 'latex')
-xlabel('Window Size $w$')
-ylabel('$\frac{r}{s}$')
-title(strcat('Minimum feature resolution for', ...
-    {' '}, string(err_level*100), '\% mean error', ' given', {' '}, ...
-    string(100*props(1)), '-', string(100*props(end)), '\% noise'))
+if plot_abs
+    figure;
+    errorbar(windows, abs(dK), dK_sd, 'ko', 'MarkerFaceColor', 'black', 'LineWidth', 1)
+    hold on
+    errorbar(windows, abs(dK_box), dK_sd_box, 'ko', 'MarkerFaceColor', 'red', 'LineWidth', 1)
+    hold on
+    errorbar(windows, abs(dK_gss), dK_sd_gss, 'ko', 'MarkerFaceColor', 'blue', 'LineWidth', 1)
+    
+    xticks(windows)
+    legend({'unfiltered', 'box', 'Gaussian'})
+    xlabel('Window size')
+    ylabel('Normalized error')
+    title('Absolute KE error with noise')
+end
