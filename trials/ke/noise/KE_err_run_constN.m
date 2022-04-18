7205b21f23db8288f3d7aa5cc9ae9c09bb9a0760
Derek Li <lij@winchesterthurston.org>
2022-04-17 20:10:23 -0700

    added comments

--- trials/ke/noise/KE_err_run_constN.m
+++ trials/ke/noise/KE_err_run_constN.m
@@ -1,5 +1,8 @@
 function [dK, dK_box, dK_gss, dK0, bias_box, bias_gss, dK_sd, dK_sd_box, dK_sd_gss, vf] = ...
     KE_err_run_constN(vf, props, K0, KEf, num_ite, window_params, display_plots)
+% A wrapper for 'KE_err_run.m' for the special case that only one noise
+% level is specified, i.e. props = [0 n], where n is the level of noise in
+% proportion to the mean speed.
 
 if ~exist('display_plots', 'var')
     display_plots = 0;
